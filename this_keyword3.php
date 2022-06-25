6d0a7eff8af6c7c0054db64e9be4f5b830e99f6e
Ishini Hathurusinghe <101358580+ishinihathurusinghe@users.noreply.github.com>
2022-06-25 17:18:28 +0530

    Add files via upload

---
+++ this_keyword3.php
@@ -0,0 +1,31 @@
+<?php
+
+class Product{
+    var $product_name;
+    var $cost_price;
+    var $sales_price;
+
+    function calculate_sales_price(){
+         $this->sales_price = round($this-> cost_price=1.2,2);
+    }
+}
+
+class Computer extends Product{
+
+
+}
+
+$my_product = new Computer;
+$my_product->product_name ='apple macbook air 2020';
+$my_product->cost_price = 190000;
+$my_product-> calculate_sales_price();
+
+$my_array = get_object_vars($my_product);
+
+echo 'object variables for my_product';
+echo '<pre>';
+print_r($my_array);
+echo '</pre>';
+
+
+?>
